1df9c34f8312285ecedc61b76802487b42758f67
thy <tianhongyuan519@163.com>
2020-12-16 15:13:44 +0800

    第一次idea提交 src

---
+++ src/com/company/hello.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @author ：Thy
+ * @date ：Created in 2020/12/16 14:24
+ */
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(2);
+    }
+}
